4161afd26a610e4582873450838d6ac46517af82
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-18 18:36:40 -0500

    Commented further study

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -135,28 +135,28 @@ class BinarySearchTree {
 
   }
 
-  /** Further Study!
-   * remove(val): Removes a node in the BST with the value val.
-   * Returns the removed node. */
+  // /** Further Study!
+  //  * remove(val): Removes a node in the BST with the value val.
+  //  * Returns the removed node. */
 
-  remove(val) {
+  // remove(val) {
 
-  }
+  // }
 
-  /** Further Study!
-   * isBalanced(): Returns true if the BST is balanced, false otherwise. */
+  // /** Further Study!
+  //  * isBalanced(): Returns true if the BST is balanced, false otherwise. */
 
-  isBalanced() {
+  // isBalanced() {
 
-  }
+  // }
 
-  /** Further Study!
-   * findSecondHighest(): Find the second highest value in the BST, if it exists.
-   * Otherwise return undefined. */
+  // /** Further Study!
+  //  * findSecondHighest(): Find the second highest value in the BST, if it exists.
+  //  * Otherwise return undefined. */
 
-  findSecondHighest() {
+  // findSecondHighest() {
     
-  }
+  // }
 }
 
 module.exports = BinarySearchTree;
